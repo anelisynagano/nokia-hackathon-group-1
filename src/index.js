0695ec8e5e3b5cf347da1322203caac87da58538
WildCodeSchool <anelisynagano@gmail.com>
2020-09-14 15:17:21 +0100

    hashrouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
       <ScrollToTop />
       <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 

e34effb840da985446f057b09ca8309751ab00bc
WildCodeSchool <anelisynagano@gmail.com>
2020-09-14 13:43:57 +0100

    Updates

---
+++ precache-manifest.aa0ae4a32b2b558de7a91d0e780fc447.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c5bb0566a8237a4227b225ec3adc75",
+    "url": "/nokia-hackathon-group-1/index.html"
+  },
+  {
+    "revision": "387bf6981dad0ef0cd49",
+    "url": "/nokia-hackathon-group-1/static/css/main.68f24f4d.chunk.css"
+  },
+  {
+    "revision": "13a82ae8db9191f2a736",
+    "url": "/nokia-hackathon-group-1/static/js/2.3fedd0ee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nokia-hackathon-group-1/static/js/2.3fedd0ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387bf6981dad0ef0cd49",
+    "url": "/nokia-hackathon-group-1/static/js/main.8920b3a6.chunk.js"
+  },
+  {
+    "revision": "5facac4381511b47621f",
+    "url": "/nokia-hackathon-group-1/static/js/runtime-main.42620524.js"
+  },
+  {
+    "revision": "52a451b4ebce322d91005829ab035998",
+    "url": "/nokia-hackathon-group-1/static/media/1.52a451b4.jpg"
+  },
+  {
+    "revision": "830ff5f6d9facf8561984b8e4071258b",
+    "url": "/nokia-hackathon-group-1/static/media/2.830ff5f6.jpg"
+  },
+  {
+    "revision": "5c9b115b75805e97e6f18a487d4d887a",
+    "url": "/nokia-hackathon-group-1/static/media/3.5c9b115b.jpg"
+  },
+  {
+    "revision": "ca4a835567b564afc9db4bb05275bcbb",
+    "url": "/nokia-hackathon-group-1/static/media/4.ca4a8355.jpg"
+  },
+  {
+    "revision": "54f5f036ceff481e3e0a914753ebe364",
+    "url": "/nokia-hackathon-group-1/static/media/5.54f5f036.jpg"
+  },
+  {
+    "revision": "29db2c31b5b7962dc74ca26f74444786",
+    "url": "/nokia-hackathon-group-1/static/media/HELP-logo.29db2c31.gif"
+  },
+  {
+    "revision": "e1f1cdc9c8afc51e38aef9114893209f",
+    "url": "/nokia-hackathon-group-1/static/media/NokiaPureHeadline_Bd.e1f1cdc9.ttf"
+  },
+  {
+    "revision": "2cb2263e964fdc30c500094435cc3b30",
+    "url": "/nokia-hackathon-group-1/static/media/NokiaPureHeadline_Lt.2cb2263e.ttf"
+  },
+  {
+    "revision": "d10dc635a994f5d8ce20b91d4273705d",
+    "url": "/nokia-hackathon-group-1/static/media/NokiaPureHeadline_Rg.d10dc635.ttf"
+  },
+  {
+    "revision": "2bf9c690596483e25886e84275c68213",
+    "url": "/nokia-hackathon-group-1/static/media/NokiaPureHeadline_ULt.2bf9c690.ttf"
+  },
+  {
+    "revision": "dfd8757f6aab67e6125c3575dfb79577",
+    "url": "/nokia-hackathon-group-1/static/media/NokiaPureHeadline_XBd..dfd8757f.ttf"
+  },
+  {
+    "revision": "d3eadc8d2baa41ab6ffebac26a3cf6b3",
+    "url": "/nokia-hackathon-group-1/static/media/NokiaPureTWeb_Bd.d3eadc8d.ttf"
+  },
+  {
+    "revision": "abb1b26db4e48c3b62940e45b06d87f9",
+    "url": "/nokia-hackathon-group-1/static/media/NokiaPureTWeb_Lt.abb1b26d.ttf"
+  },
+  {
+    "revision": "536160e84aba3fb83504066dd068bed3",
+    "url": "/nokia-hackathon-group-1/static/media/NokiaPureTWeb_Rg.536160e8.ttf"
+  },
+  {
+    "revision": "762af741c73c64ebdc4992e0d5208077",
+    "url": "/nokia-hackathon-group-1/static/media/PhonesForEveryoneBanner.762af741.png"
+  },
+  {
+    "revision": "ffdf93c352b5888d1730c8dc9c8d4008",
+    "url": "/nokia-hackathon-group-1/static/media/TopBanner.ffdf93c3.jpg"
+  },
+  {
+    "revision": "7318768047de252e999fcf4c3a057f2b",
+    "url": "/nokia-hackathon-group-1/static/media/YourMatch.73187680.jpg"
+  }
+]);
